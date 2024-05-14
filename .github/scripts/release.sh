eda0c2d1a1e842920ec7b16877d2d4706af56a42
abhishek-engg-mobilesdk <abhishek.kumar@moengage.com>
2024-05-14 14:38:14 +0530

    Printing Secrets

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -3,4 +3,10 @@ git checkout master
 git pull origin master
 ./gradlew assemble --stacktrace
 cd sdk-logger
+echo $OSS_PASSWORD
+echo $OSS_STAGING_PROFILE_ID
+echo $OSS_SIGNING_KEY_ID
+echo $OSS_SIGNING_PASSWORD
+echo $OSS_SIGNING_KEY
+echo $TEST
 ../gradlew publish --no-daemon --no-parallel
